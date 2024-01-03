c8f901578843c9dfa280cf8afb4b799bea57d531
Swodhinn <130154840+Swodhinn@users.noreply.github.com>
2024-01-03 21:07:32 +0545

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 number");
+        a= sc.nextInt();
+        b= sc.nextInt();
+        int c=a+b;
+        System.out.println(c);
+    }
+
+}
